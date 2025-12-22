641ebc749e3c617b138da420cd80c7d8ddfb62bb
WhiteJbb <gunni6112@gmail.com>
2025-12-22 17:39:48 +0900

    fix: move RSS fetching to server-side to resolve CORS issues

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,7 +1,9 @@
-"use client";
-
 import { BlogClient } from "./BlogClient";
+import { fetchTistoryPosts } from "@/lib/tistory";
 
-export default function BlogPage() {
-  return <BlogClient posts={[]} />;
+export default async function BlogPage() {
+  // 서버 사이드에서 RSS 데이터 가져오기
+  const posts = await fetchTistoryPosts();
+  
+  return <BlogClient posts={posts} />;
 }
